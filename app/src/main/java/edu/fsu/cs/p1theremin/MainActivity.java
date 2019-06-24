bdf6f53e7770fac5bc5cc844c0cb3e3c0a235b86
Elion Sickler <es17m@fsu.edu>
2019-06-24 18:55:09 -0400

    Initial commit - Have implemented the UI for the toggle button, octave slider, and a small menu. The square wave sample is included in the project under raw/.

---
+++ app/src/main/java/edu/fsu/cs/p1theremin/MainActivity.java
@@ -0,0 +1,83 @@
+package edu.fsu.cs.p1theremin;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        final SeekBar octaveSelector = findViewById(R.id.octaveBar);
+
+        octaveSelector.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    if (progress <= 12) {
+                        octaveSelector.setProgress(0);
+                    }
+                    else if (progress < 38) {
+                        octaveSelector.setProgress(25);
+                    }
+                    else if (progress < 63) {
+                        octaveSelector.setProgress(50);
+                    }
+                    else if (progress < 88) {
+                        octaveSelector.setProgress(75);
+                    }
+                    else {
+                        octaveSelector.setProgress(100);
+                    }
+                }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+}
