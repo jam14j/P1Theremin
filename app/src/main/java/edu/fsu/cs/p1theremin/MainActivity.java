14911a41505bb8175120dbbf13db5da1aba33881
Elion Sickler <es17m@fsu.edu>
2019-07-01 16:49:25 -0400

    Added volume control along the X-axis and the sound repeats on button toggle. Added coordinate logging from gyroscope to LogCat. Still working on calculation for position.

--- app/src/main/java/edu/fsu/cs/p1theremin/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/p1theremin/MainActivity.java
@@ -1,50 +1,105 @@
 package edu.fsu.cs.p1theremin;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.AudioManager;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    private SensorManager mSensorManager;
-    private Sensor mAccelerometer;
+
+public class MainActivity extends AppCompatActivity  {
+
+    public Messenger messenger;
+    private ToggleButton playButton;
+    private Button resetButton;
+
+    private ServiceConnection thereminConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            messenger = new Messenger(iBinder);
+        }
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            messenger = null;
+        }
+    };
+
+    protected void onDestroy (Bundle savedInstanceState) {
+        super.onDestroy();
+        unbindService(thereminConnection);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        playButton = (ToggleButton) findViewById(R.id.thereminButton);
+        resetButton = (Button) findViewById(R.id.button);
+        Intent soundIntent = new Intent(this, ThereminService.class);
+        bindService(soundIntent, thereminConnection, Context.BIND_AUTO_CREATE);
 
         final SeekBar octaveSelector = findViewById(R.id.octaveBar);
+        playButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Message msg;
+                if (isChecked) msg = Message.obtain(null, ThereminService.MSG_PLAY, 0, 0);
+                else msg = Message.obtain(null, ThereminService.MSG_STOP, 0, 0);
+                try {messenger.send(msg);}
+                catch (RemoteException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
 
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Message msg = Message.obtain(null, ThereminService.MSG_RESET, 0, 0);
+                try {messenger.send(msg);}
+                catch (RemoteException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
         octaveSelector.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                    if (progress <= 12) {
-                        octaveSelector.setProgress(0);
-                    }
-                    else if (progress < 38) {
-                        octaveSelector.setProgress(25);
-                    }
-                    else if (progress < 63) {
-                        octaveSelector.setProgress(50);
-                    }
-                    else if (progress < 88) {
-                        octaveSelector.setProgress(75);
-                    }
-                    else {
-                        octaveSelector.setProgress(100);
-                    }
+                if (progress <= 12) {
+                    octaveSelector.setProgress(0);
+                } else if (progress < 38) {
+                    octaveSelector.setProgress(25);
+                } else if (progress < 63) {
+                    octaveSelector.setProgress(50);
+                } else if (progress < 88) {
+                    octaveSelector.setProgress(75);
+                } else {
+                    octaveSelector.setProgress(100);
                 }
-
+                if (octaveSelector.getProgress() != progress)
+                    switch (octaveSelector.getProgress()) {
+                        case 0:
+                        case 25:
+                        case 50:
+                        case 75:
+                        case 100:
+                    }
+            }
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
 
@@ -55,29 +110,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
             }
         });
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.mainmenu, menu);
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
         return true;
-    }
-
-    protected void onResume() {
-        super.onResume();
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    protected void onPause() {
-        super.onPause();
-        mSensorManager.unregisterListener(this);
-    }
-
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-
-    public void onSensorChanged(SensorEvent event) {
-
     }
 }
--- app/src/main/java/edu/fsu/cs/p1theremin/ThereminService.java
+++ app/src/main/java/edu/fsu/cs/p1theremin/ThereminService.java
@@ -1,77 +1,109 @@
 package edu.fsu.cs.p1theremin;
 
-import android.app.IntentService;
-import android.content.Intent;
+import android.app.Service;
 import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.util.Log;
 
 
-public class ThereminService extends IntentService {
-    // TODO: Rename actions, choose action names that describe tasks that this
-    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-    private static final String ACTION_FOO = "edu.fsu.cs.p1theremin.action.FOO";
-    private static final String ACTION_BAZ = "edu.fsu.cs.p1theremin.action.BAZ";
+public class ThereminService extends Service implements SensorEventListener {
 
-    // TODO: Rename parameters
-    private static final String EXTRA_PARAM1 = "edu.fsu.cs.p1theremin.extra.PARAM1";
-    private static final String EXTRA_PARAM2 = "edu.fsu.cs.p1theremin.extra.PARAM2";
+    public static final int MSG_PLAY = 1;
+    public static final int MSG_STOP = 2;
+    public static final int MSG_RESET = 3;
+    SensorManager sensorManager;
+    Sensor gyroscope;
+    int soundID;
+    private SoundPool sound;
+    final Messenger serviceMessenger = new Messenger(new IncomingHandler());
+    boolean activeTheremin = false;
+    int sensorLog = 0;
+    double x, y, z; // These are position variables, not badly-named variables (maybe)
+    AudioManager am;
+    public ThereminService() {}
 
-    public ThereminService() {
-        super("ThereminService");
-    }
-
-    /**
-     * Starts this service to perform action Foo with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionFoo(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, ThereminService.class);
-        intent.setAction(ACTION_FOO);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
-    /**
-     * Starts this service to perform action Baz with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionBaz(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, ThereminService.class);
-        intent.setAction(ACTION_BAZ);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
-    @Override
-    protected void onHandleIntent(Intent intent) {
-        if (intent != null) {
-            final String action = intent.getAction();
-            if (ACTION_FOO.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionFoo(param1, param2);
-            } else if (ACTION_BAZ.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionBaz(param1, param2);
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_PLAY:
+                    if (! activeTheremin) soundID = sound.play(soundID, 1.0f, 1.0f, 0, -1, 1.0f);
+                    else sound.resume(soundID);
+                    activeTheremin = true;
+                    registerGyro();
+                    break;
+                case MSG_STOP:
+                    sound.pause(soundID);
+                    unregisterGyro();
+                    break;
+                case MSG_RESET:
+                    x = 0;
+                    y = 0;
+                    z = 0;
+                    break;
             }
         }
     }
 
-    private void handleActionFoo(String param1, String param2) {
-        // TODO: Handle action Foo
-        throw new UnsupportedOperationException("Not yet implemented");
+    @Override
+    public IBinder onBind(Intent intent) {
+        am = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+        sound = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        soundID = sound.load(this, R.raw.boop, 1);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        return serviceMessenger.getBinder();
     }
 
-    private void handleActionBaz(String param1, String param2) {
-        // TODO: Handle action Baz
-        throw new UnsupportedOperationException("Not yet implemented");
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return true;
+    }
+    @Override
+    public void onCreate() { super.onCreate(); }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+    int volume;
+    public void onSensorChanged(SensorEvent event) {
+        x += getPositionFromVelocity(event.values[0]);
+        y += getPositionFromVelocity(event.values[1]);
+        z += getPositionFromVelocity(event.values[2]);
+        /*Log.i ("VALUES", "Run: " + sensorLog +
+                "\nX: " + x +
+                "\nY: " + y +
+                "\nZ: " + z + '\n');
+        sensorLog++;
+*/
+        if (x < 0) volume = 0;
+        else if (x > 100) volume = 100;
+        else volume = (int) x;
+        am.setStreamVolume(AudioManager.STREAM_MUSIC, volume / 6, 0);
+
+    }
+    public double getPositionFromVelocity(double vel) {
+        return  vel / 4              // This is the derivative to convert velocity to position (rad/s => rad)
+                * 180 / Math.PI;    // This converts radians to degrees                        (rad => °)
+    }
+
+    public void registerGyro() {
+        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void unregisterGyro() {
+        sensorManager.unregisterListener(this);
     }
 }
